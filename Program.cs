5ae42a84a9ed1474d7d69ae89aadd4282916e665
jamesnitz <jamesanitz2@gmail.com>
2020-02-28 09:30:45 -0600

    while loops

--- Program.cs
+++ Program.cs
@@ -1,29 +1,57 @@
 ﻿using System;
 
-namespace GuessingGame {
-    class Program {
-        static void Main (string[] args) {
-            Random rand = new Random ();
-            int secretNumber = rand.Next (1, 11);
+namespace GuessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random rand = new Random();
+      int secretNumber = rand.Next(1, 11);
+      bool isCorrect = false;
+      // For Loop
+      // for (int i = 0; i < 3; i++)
+      // {
+      //   Console.WriteLine("guess between 1 and 10");
+      //   string userGuess = Console.ReadLine();
+      //   int userGuessInt = int.Parse(userGuess);
+      //   if (userGuessInt == secretNumber)
+      //   {
+      //     Console.WriteLine($"You guessed it! It was {secretNumber}");
+      //     return;
+      //   }
+      //   else if (userGuessInt < secretNumber && i != 2)
+      //   {
+      //     Console.WriteLine("Nope guess higher");
+      //   }
+      //   else if (userGuessInt > secretNumber && i != 2)
+      //   {
+      //     Console.WriteLine("Nope guess lower");
 
-            for (int i = 0; i < 3; i++) {
-                Console.WriteLine ("guess between 1 and 10");
-                string userGuess = Console.ReadLine ();
-                int userGuessInt = int.Parse (userGuess);
-                if (userGuessInt == secretNumber) {
-                    Console.WriteLine ($"You guessed it! It was {secretNumber}");
-                    return;
-                } else if (userGuessInt < secretNumber && i != 2) {
-                    Console.WriteLine ("Nope guess higher");
-                } else if (userGuessInt > secretNumber && i != 2) {
-                    Console.WriteLine ("Nope guess lower");
+      //   }
+      //   else
+      //   {
+      //     Console.WriteLine("You're WRONGGGGGG");
 
-                } else {
-                    Console.WriteLine ("You're WRONGGGGGG");
+      //   }
 
-                }
-            }
-
+      //WHILE LOOP
+      while (!isCorrect)
+      {
+        Console.WriteLine("guess between 1 and 10");
+        string userGuess = Console.ReadLine();
+        int userGuessInt = int.Parse(userGuess);
+        if (userGuessInt == secretNumber)
+        {
+          Console.WriteLine($"You guessed it! It was {secretNumber}");
+          break;
         }
+        else
+        {
+          Console.WriteLine("WRONG");
+        }
+      }
+
     }
+  }
 }
